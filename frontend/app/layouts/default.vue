306951e96b4380c1a968c357a3213e3f8e300a8b
Akram Fadel <af8356207@gmail.com>
2026-02-20 23:32:54 +0100

    created app layout

---
+++ frontend/app/layouts/default.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <SidebarProvider >
+        <AppSidebar />
+        <main class="w-full">
+            <nav class="flex justify-between items-center p-4 w-full border-b">
+                <SidebarTrigger />
+                <ModeToggle />
+
+            </nav>
+            <slot />
+        </main>
+    </SidebarProvider>
+</template>
