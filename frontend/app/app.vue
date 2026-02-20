6ffeb8afa360e5b31880d84e755291ae5af00038
Akram Fadel <af8356207@gmail.com>
2026-02-20 23:33:36 +0100

    updated app.vue to trigger splash screen removal

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,6 +1,20 @@
+<script setup>
+import { onMounted } from 'vue'
+
+onMounted(async () => {
+  await nextTick()
+
+  try {
+    const { invoke } = await import('@tauri-apps/api/core')
+    await invoke('splash_screen')
+  } catch (e) {
+    console.log('Not in Tauri context')
+  }
+})
+</script>
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtLoadingIndicator />
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
