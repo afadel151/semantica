e38b2acd4e5c21c376fe30296fcfdad1aaf1152d
Akram Fadel <af8356207@gmail.com>
2026-02-20 15:41:11 +0100

    installed shadcn-vue

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,11 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
+  css: ['~/assets/css/tailwind.css'],
   ssr: false,
+
   runtimeConfig: {
     public: {
       apiBase: process.env.API_BASE_URL || 'http://localhost:8000'
     }
   },
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  modules: ['shadcn-nuxt'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component.
+     * @default "Ui"
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * Will respect the Nuxt aliases.
+     * @link https://nuxt.com/docs/api/nuxt-config#alias
+     * @default "@/components/ui"
+     */
+    componentDir: '@/components/ui'
+  },
+
 })
