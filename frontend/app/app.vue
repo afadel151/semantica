1e58d1f05f55f18969fe49c863ce1524dcdee91d
Akram Fadel <af8356207@gmail.com>
2026-03-02 18:03:39 +0100

    configured colorTheme to prefer system s theme

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -3,13 +3,15 @@ import { onMounted } from 'vue'
 
 onMounted(async () => {
   await nextTick()
-
+  
   try {
     const { invoke } = await import('@tauri-apps/api/core')
     await invoke('splash_screen')
   } catch (e) {
     console.log('Not in Tauri context')
   }
+  const colorMode = useColorMode()
+  colorMode.preference = 'system'
 })
 </script>
 <template>
