eb0a7ef8ba1caeafab29b4d5374df3fb073a26d4
Akram Fadel <af8356207@gmail.com>
2026-03-02 17:57:35 +0100

    removed Theme mode toggle with the navbar from layout

--- frontend/app/layouts/default.vue
+++ frontend/app/layouts/default.vue
@@ -7,13 +7,13 @@ const pageName: string| unknown = route.meta.title
     <SidebarProvider >
         <AppSidebar />
         <main class="w-full">
-            <nav class="flex justify-between items-center p-4 w-full border-b">
+            <!-- <nav class="flex justify-between items-center p-4 w-full border-b"> -->
                 <SidebarTrigger :title="pageName"  />
                 
-                <ModeToggle />
+                <!-- <ModeToggle /> -->
 
-            </nav>
-            <slot />
+            <!-- </nav> -->
+            <slot />    
         </main>
     </SidebarProvider>
 </template>
